6071162852a17f4273d946a9501ace21184b4a37
machi0x <machi0545@hotmail.com>
2025-12-30 20:06:30 +0900

    Fix build error.

--- app/src/main/java/com/machi/memoiz/ui/screens/SettingsScreen.kt
+++ app/src/main/java/com/machi/memoiz/ui/screens/SettingsScreen.kt
@@ -8,6 +8,11 @@ import android.provider.Settings
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.CheckCircle
+import androidx.compose.material.icons.filled.Info
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
