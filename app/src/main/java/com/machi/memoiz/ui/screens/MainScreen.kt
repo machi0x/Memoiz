671c2a16f45c12fc64f60bf2bd6ea6eb3bd4d674
machi0x <machi0545@hotmail.com>
2025-12-30 16:47:35 +0900

    Don't crop side panel top image

--- app/src/main/java/com/machi/memoiz/ui/screens/MainScreen.kt
+++ app/src/main/java/com/machi/memoiz/ui/screens/MainScreen.kt
@@ -250,13 +250,21 @@ private fun NavigationDrawerContent(
         }
 
         // Banner image
+        val bannerPainter = painterResource(id = R.drawable.top_banner)
+        val bannerAspectRatio = bannerPainter.intrinsicSize.let { size ->
+            val width = size.width
+            val height = size.height
+            if (width > 0 && height > 0) width / height else 3.5f
+        }
         Image(
-            painter = painterResource(id = R.drawable.top_banner),
+            painter = bannerPainter,
             contentDescription = null,
             modifier = Modifier
                 .fillMaxWidth()
-                .height(80.dp),
-            contentScale = ContentScale.Crop
+                .aspectRatio(bannerAspectRatio)
+                .padding(horizontal = 16.dp)
+                .clip(RoundedCornerShape(12.dp)),
+            contentScale = ContentScale.Fit
         )
 
         HorizontalDivider()
