4b3d95861fb360b6d56f1c9685db50b832442f21
machi0x <machi0545@hotmail.com>
2026-01-01 00:49:03 +0900

    Fix Settings UI oss menu issue

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -177,7 +177,7 @@ dependencies {
     implementation(platform("com.google.firebase:firebase-bom:34.7.0"))
     implementation("com.google.firebase:firebase-analytics")
     implementation("com.google.firebase:firebase-crashlytics")
-    implementation("com.google.android.gms:play-services-oss-licenses:17.0.1")
+    implementation("com.google.android.gms:play-services-oss-licenses:17.2.1")
 
     // Testing
     testImplementation("junit:junit:4.13.2")
