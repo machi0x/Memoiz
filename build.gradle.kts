62956b392b8f8d44bc8a572b8e4041f466b6febf
machi0x <machi0545@hotmail.com>
2026-01-06 23:09:14 +0900

    Update AGP

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,7 @@ buildscript {
 
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "8.6.0" apply false
+    id("com.android.application") version "8.13.2" apply false
     id("org.jetbrains.kotlin.android") version "2.0.0" apply false
     id("com.google.devtools.ksp") version "2.0.0-1.0.21" apply false
     // Compose Gradle plugin to provide Compose compiler plugin for Kotlin 2.x
