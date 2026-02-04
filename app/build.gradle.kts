69dc3239ee28ca9f1d050bb8f347000fc6c773f8
machi0x <machi0545@hotmail.com>
2026-02-04 22:10:40 +0900

    Change version to 1.2.5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,7 +43,7 @@ android {
         targetSdk = 36
         val gitCount = getGitCommitCount()
         versionCode = gitCount
-        versionName = "1.2.4"
+        versionName = "1.2.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
